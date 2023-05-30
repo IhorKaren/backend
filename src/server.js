ff70086ba326ec6fa1a646f9b7c60472671240b0
Ihor Karen <karenihor@gmail.com>
2023-05-31 00:29:16 +0300

    fix?

--- src/server.js
+++ src/server.js
@@ -1,12 +1,7 @@
-const { MongoClient } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || config.get("serverPort");
-
-require("dotenv").config();
-
-const client = new MongoClient(process.env.MONGO_DB_KEY);
 
 app.use(express.json());
 app.use(cors());
